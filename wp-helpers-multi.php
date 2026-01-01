dfba21067e0b93b8e361be80afaa07f15aeacf85
aki-sho <inpalmark@gmail.com>
2026-01-01 18:35:34 +0900

    Initial skeleton

---
+++ wp-helpers-multi.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WP Helpers Multi
+ * Description: Utility tools plugin.
+ * Version: 0.1.0
+ * Author: Your Name
+ * Text Domain: wp-helpers-multi
+ */
+if (!defined('ABSPATH')) exit;
