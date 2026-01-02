27648a26395ab69768bec29da82486c4eef4472e
aki-sho <inpalmark@gmail.com>
2026-01-02 22:25:47 +0900

    Add new files

--- wp-helpers-multi.php
+++ wp-helpers-multi.php
@@ -3,6 +3,8 @@
  * Plugin Name: WP Helpers Multi
  * Description: Utility tools plugin.
  * Version: 0.1.0
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
  * Author: Your Name
  * Text Domain: wp-helpers-multi
  */
