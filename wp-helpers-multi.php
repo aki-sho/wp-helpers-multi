4ff06e63e7c542f401c7df0a8187150f840428dc
aki-sho <128263173+aki-sho@users.noreply.github.com>
2026-01-04 04:44:17 +0900

    Bump version to 0.1.1
    Commit directly to the main branch

--- wp-helpers-multi.php
+++ wp-helpers-multi.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Helpers Multi
  * Description: Utility tools plugin.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Requires at least: 6.0
  * Requires PHP: 7.4
  * Author: Your Name
